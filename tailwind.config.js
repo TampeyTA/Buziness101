65645699b423df8672b42337400402f7e0bd5ee0
TampeyTA <152615220+TampeyTA@users.noreply.github.com>
2024-12-31 23:36:09 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-import {nextui} from '@nextui-org/theme'
+const { nextui } = require('@nextui-org/theme')
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
@@ -18,3 +18,4 @@ module.exports = {
   darkMode: "class",
   plugins: [nextui()],
 }
+
